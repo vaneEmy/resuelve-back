2e07d0b687ad14d8b8e8c1b8dac4a807e39ab10a
Vanessa García <vanvanemily@gmail.com>
2021-03-12 16:14:07 -0600

    Describe methods in services.

--- app/teams/services.py
+++ app/teams/services.py
@@ -7,7 +7,7 @@ from .team_model import Team
 
 def get_min_goal_by_team(team: str, level: str):
     """
-
+        Get the name of the team, the name and minimum goals of the level by the team and level provided 
     """
 
     return db.session.query(Team.name, Level.name, Level.goal).join(Level).filter(Team.name == team, Level.name == level)
