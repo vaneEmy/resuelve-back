fa1655b04bd0a1372d1a87d97a99b625587e8ed7
Vanessa García <vanvanemily@gmail.com>
2021-03-12 21:49:46 -0600

    Update readme file of the request and adding more comments of the code.

--- app/players/endpoints.py
+++ app/players/endpoints.py
@@ -11,33 +11,36 @@ playersRouter = APIRouter()
 @playersRouter.post('/salaries/calculation')
 def calculate_salaries(players: PlayersSchema):
     
+    # We create a dictionary of teams which every team will have players with some information and we get the minimum goals by their level
     teams = dict()
     new_players_list = []
     
-    # We create a dictionary of teams which every team has players with their information and the  minimum goals by their level
+    # We iterate over the list of players given
     for player in players.jugadores:
         
         # Validate if the team exists
         if not team_already_exists(player.equipo):
             raise HTTPException(status_code=422, detail=f"Team {player.equipo} does not exist")
         
-        # Validate if the level exists
+        # We create a player object if return None it's because there isn't exist the level for the team
         player_query = create_player(player.nombre, player.nivel, player.goles, player.sueldo, player.bono, player.equipo)
         if player_query is None:
             raise HTTPException(status_code=422, detail=f"The level {player.nivel} does not exists for the team {player.equipo}")
         
         # We verify if the team exists in the key of the dictionary 
         if teams.get(player.equipo):
+            # Due to the team is in the dictionary, we get the list of players and append the new player
             players_list = teams.get(player.equipo)
             players_list.append(player_query)
             teams[player.equipo] = players_list
         else:
+            # We create a new list of player to the dictionary
             players_list = []
             players_list.append(player_query)
             teams[player.equipo] = players_list
 
     
-    # We iterate over the teams dictionary in order to calculate the total of goals by players in the team, the bono and the total salary 
+    # We iterate over the teams dictionary in order to calculate the total of team goals by players in the team, the bono and the total salary 
     for players_values in teams.values():
         
         # We sum all goals and minimum goals by team  
@@ -46,7 +49,7 @@ def calculate_salaries(players: PlayersSchema):
         
         team_porcentage = (team_goals*100)/total_goals
         
-        # Add players list to a new list
+        # Add players list to a new list, we use it at the end
         new_players_list.extend(players_values)
         
         for player_value in players_values:
