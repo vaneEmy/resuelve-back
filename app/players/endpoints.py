a20f33440c825ce4694087fe042426027f2ce03c
Vanessa García <vanvanemily@gmail.com>
2021-03-12 19:22:40 -0600

    Return a new list of players with the total salary.

--- app/players/endpoints.py
+++ app/players/endpoints.py
@@ -10,6 +10,7 @@ playersRouter = APIRouter()
 def calculate_salaries(players: PlayersSchema):
     
     teams = dict()
+    new_players_list = []
     
     # We create a dictionary of teams which every team has players with their information and the  minimum goals by their level
     for player in players.jugadores:
@@ -27,24 +28,28 @@ def calculate_salaries(players: PlayersSchema):
     # We iterate over the teams dictionary in order to calculate the total of goals by players in the team, the bono and the total salary 
     for players_values in teams.values():
         
+        # We sum all goals and minimum goals by team  
         team_goals = sum(p.get('goles') for p in players_values)
         total_goals = sum(p.get('goles_minimos') for p in players_values)
         
         team_porcentage = (team_goals*100)/total_goals
 
+        # Add players list to a new list
+        new_players_list.extend(players_values)
+        
         for player_value in players_values:
             individual_porcentage = (player_value.get('goles')*100) / player_value.get('goles_minimos')
             
+            # Calculate team and individual 
             team_goal = ((player_value.get('bono') / 2) * team_porcentage) / 100
             individual_goal = ((player_value.get('bono') / 2) * individual_porcentage) / 100
             
             total_bono = team_goal +  individual_goal
 
-            total_salary = player_value.get('sueldo') + total_bono
+            # Calculate the total salary and round decimals
+            total_salary = round(player_value.get('sueldo') + total_bono, 2)
             
             # Add total salary to player object
             player_value["sueldo_completo"] = total_salary
 
-            print(player_value.get('sueldo_completo'))
-            
-    return { "message": "Ok"}
+    return { "jugadores": new_players_list}
